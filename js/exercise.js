6650b6803f239384bdeeaf766152a841c7213bca
Jonathan Casson <jonathancasson@Jonathans-MacBook-Pro-2.local>
2018-03-16 10:49:37 +1100

    code more readable

--- js/exercise.js
+++ js/exercise.js
@@ -1,228 +1,184 @@
+//Make the DIV element draggagle:
+dragElement(document.getElementById(("mydiv")));
+
+function dragElement(elmnt) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+//End CSS manipulation
+
+//The folllowing are global arrays
 let xMoves = "";
 let oMoves = "";
 let moves = "";
+let whatWasClicked = "";
+let turns = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let xscore = 0
+let oscore = 0
 
-let turns = ['1','2','3','4','5','6','7','8','9']
-
-function reply_click()
+function reply_click() //says which specific button was clicked
 {
-    let whatWasClicked = event.srcElement.id;
+    whatWasClicked = event.srcElement.id;
+};
+const playAudio = function() {
+    $('#drawing')[0].play(); //declaring array necessary in function
+}
+const playAudioTwo = function() {
+    $('#drawingTwo')[0].play();
+}
+const playerTurn = function() {
+    if (turns[turns.length - 1] % 2 === 0) { // if even
+        $('p').text("O's turn"); //change turn text
+
+    } else {
+        $('p').text("X's turn");
+    }
+}
+const win = function() {
+    $('.buttons *').attr('disabled', true);
+    if (turns[turns.length - 1] % 2 === 0) {
+        oscore = oscore + 1; // If turn number even, O wins
+        $('.x').addClass('oWins'); //Change O's to change style
+        $('.oscore').hide(10) //for score animation
+        $('.oscore').text(oscore);
+        $('.oscore').show(1000);
+        $('p').text("O WINS !!!"); //Change turn text to show congratulations
+    } else {
+        xscore = xscore + 1;
+        $('.o').addClass('xWins');
+        $('.xscore').hide(10)
+        $('.xscore').text(xscore);
+        $('.xscore').show(1000)
+        $('p').text("X WINS !!!");
+    }
 }
 
-const winCheck = function(){
-  if (turns.length % 2 === 0){
-    moves = xMoves;
-  }
-  else {
-    moves = oMoves;
-  }
-  if(moves.indexOf('1') > -1 && moves.indexOf('2') > -1 && moves.indexOf('3') > -1){
-    console.log(`yesssss`);
-  }
+const refresh = function() {
+    xMoves = "";
+    oMoves = "";
+    moves = "";
+    whatWasClicked = ""; //resets global variables
+    $('.buttons *').removeAttr("disabled")
+    $('.buttons *').removeClass("x")
+    $('.buttons *').removeClass("o")
+    $('.buttons *').removeClass("xWins")
+    $('.buttons *').removeClass("oWins") //resets classes and attributes
 
-  if(moves.indexOf('4') > -1 && moves.indexOf('5') > -1 && moves.indexOf('6') > -1){
-    console.log(`yesssss`);
-  }
+    if (turns[0] === 1) {
+        turns = [2, 3, 4, 5, 6, 7, 8, 9, 10] // so starting players rotate
+    } else {
+        turns = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    }
+};
 
-
-  if(moves.indexOf('1') > -1 && moves.indexOf('4') > -1 && moves.indexOf('7') > -1){
-    console.log(`yesssss`);
-  }
-
-
-  if(moves.indexOf('2') > -1 && moves.indexOf('5') > -1 && moves.indexOf('8') > -1){
-    console.log(`yesssss`);
-  }
-
-
-  if(moves.indexOf('3') > -1 && moves.indexOf('6') > -1 && moves.indexOf('9') > -1){
-    console.log(`yesssss`);
-  }
-
-
-  if(moves.indexOf('1') > -1 && moves.indexOf('5') > -1 && moves.indexOf('9') > -1){
-    console.log(`yesssss`);
-  }
-
-
-  if(moves.indexOf('3') > -1 && moves.indexOf('5') > -1 && moves.indexOf('7') > -1){
-    console.log(`yesssss`);
-  }
-
-
-  if(turns[0] != '1'){
-    drawMessage();
-  }
-  moves = "";
-
+const turn = function() {
+    let classWhatWasClicked = "#" + whatWasClicked; //assigns class variable to individual buttons
+    $(classWhatWasClicked).attr('disabled', true); //disables individual buttons
+    if (turns[turns.length - 1] % 2 === 0) {
+        $(classWhatWasClicked).addClass('x'); //changes background of transparent button to X image
+        $(classWhatWasClicked).hide();
+        $(classWhatWasClicked).fadeIn(1500); //animation
+        playAudio(); // plays drawing sound effect
+        xMoves = xMoves + whatWasClicked; //adds whats was clicked to xMoves global array
+    } else {
+        $(classWhatWasClicked).addClass('o');
+        oMoves = oMoves + whatWasClicked
+        $(classWhatWasClicked).hide();
+        $(classWhatWasClicked).fadeIn(1500);
+        playAudioTwo();
+    }
 }
-
-const playerTurn = function(){
-
+const winCheck = function() {
+    if (turns[turns.length - 1] % 2 === 0) {
+        moves = xMoves;
+    } else {
+        moves = oMoves;
+    }
+    if (moves.indexOf('1') > -1 && moves.indexOf('2') > -1 && moves.indexOf('3') > -1) {
+        win();
+    } else if (moves.indexOf('4') > -1 && moves.indexOf('5') > -1 && moves.indexOf('6') > -1) {
+        win();
+    } else if (moves.indexOf('7') > -1 && moves.indexOf('8') > -1 && moves.indexOf('9') > -1) {
+        win();
+    } else if (moves.indexOf('1') > -1 && moves.indexOf('4') > -1 && moves.indexOf('7') > -1) {
+        win();
+    } else if (moves.indexOf('2') > -1 && moves.indexOf('5') > -1 && moves.indexOf('8') > -1) {
+        win();
+    } else if (moves.indexOf('3') > -1 && moves.indexOf('6') > -1 && moves.indexOf('9') > -1) {
+        win();
+    } else if (moves.indexOf('1') > -1 && moves.indexOf('5') > -1 && moves.indexOf('9') > -1) {
+        win();
+    } else if (moves.indexOf('3') > -1 && moves.indexOf('5') > -1 && moves.indexOf('7') > -1) {
+        win();
+    } else if (turns.length === 1) {
+        $('.buttons *').attr('disabled', true); //all buttons are disabled
+        $('p').text("It's a draw");
+    } else {
+        moves = "";
+    }
 }
+$('h3').on('click', function() { //on click of restart button
+    playerTurn();
+    refresh();
+})
 
-
+//All game functionality within here
 $(document).ready(function() {
-  console.log(`document ready`)
+    playerTurn(); //shows whose turn it is
+    $('.xscore').text(xscore)
+    $('.oscore').text(oscore)
 
-  $('#1').on('click', function(){
-    $(this).prop('disabled', true);
-
-
-    if(turns.length % 2 === 0 ){
-      $('#1').addClass('x');
-      xMoves = xMoves + "1";
-    }
-    else{
-      $('#1').addClass('o');
-      oMoves = oMoves + "1"
-    }
-    winCheck();
-    playerTurn();
-    turns.pop();
-
-  })
-
-
-  $('#2').on('click', function(){
-    $(this).prop('disabled', true);
-
-    if(turns.length % 2 === 0 ){
-      $('#2').addClass('x');
-      xMoves = xMoves + "2";
-    }
-    else{
-      $('#2').addClass('o');
-      oMoves = oMoves + "2"
-    }
-    winCheck();
-    playerTurn();
-    turns.pop();
-
-  })
-
-
-  $('#3').on('click', function(){
-    $(this).prop('disabled', true);
-
-    if(turns.length % 2 === 0 ){
-      $('#3').addClass('x');
-      xMoves = xMoves + "3";
-    }
-    else{
-      $('#3').addClass('o');
-      oMoves = oMoves + "3"
-    }
-    winCheck();
-    playerTurn();
-    turns.pop();
-
-  })
-
-  $('#4').on('click', function(){
-    $(this).prop('disabled', true);
-
-    if(turns.length % 2 === 0 ){
-      $('#4').addClass('x');
-      xMoves = xMoves + "4";
-    }
-    else{
-      $('#4').addClass('o');
-      oMoves = oMoves + "4"
-    }
-    winCheck();
-    playerTurn();
-    turns.pop();
-
-  })
-
-
-  $('#5').on('click', function(){
-    $(this).prop('disabled', true);
-
-    if(turns.length % 2 === 0 ){
-      $('#5').addClass('x');
-      xMoves = xMoves + "5";
-    }
-    else{
-      $('#5').addClass('o');
-      oMoves = oMoves + "5"
-    }
-    winCheck();
-    playerTurn();
-    turns.pop();
-
-  })
-
-
-  $('#6').on('click', function(){
-    $(this).prop('disabled', true);
-
-    if(turns.length % 2 === 0 ){
-      $('#6').addClass('x');
-      xMoves = xMoves + "6";
-    }
-    else{
-      $('#6').addClass('o');
-      oMoves = oMoves + "6"
-    }
-    winCheck();
-    playerTurn();
-    turns.pop();
-
-  })
-
-
-  $('#7').on('click', function(){
-    $(this).prop('disabled', true);
-    moves = moves + "7";
-
-    if(turns.length % 2 === 0 ){
-      $('#7').addClass('x');
-      xMoves = xMoves + "7";
-    }
-    else{
-      $('#7').addClass('o');
-      oMoves = oMoves + "7"
-    }
-    winCheck();
-    playerTurn();
-    turns.pop();
-
-  })
-
-
-  $('#8').on('click', function(){
-    $(this).prop('disabled', true);
-
-    if(turns.length % 2 === 0 ){
-      $('#8').addClass('x');
-      xMoves = xMoves + "8";
-    }
-    else{
-      $('#8').addClass('o');
-      oMoves = oMoves + "8"
-    }
-    winCheck();
-    playerTurn();
-    turns.pop();
-
-  })
-
-  $('#9').on('click', function(){
-    $(this).prop('disabled', true);
-
-    if(turns.length % 2 === 0 ){
-      $('#9').addClass('x');
-      xMoves = xMoves + "9";
-    }
-    else{
-      $('#9').addClass('o');
-      oMoves = oMoves + "9"
-    }
-    winCheck();
-    playerTurn();
-    turns.pop();
-
-  })
+    $('.buttons').on('click', function() {
+        if (whatWasClicked.length === 0 || xMoves.indexOf(whatWasClicked) > -1 || oMoves.indexOf(whatWasClicked) > -1) {}
+         // fixes strange error if space between buttons is clicked
+        else {
+            playerTurn();
+            turn();
+            winCheck();
+            if (moves.length > 0 && turns.length < 8) {
+                turns.pop();
+                whatWasClicked = "";
+            } else {
+                turns.pop();
+                playerTurn();
+            }
+        }
+    })
 })
